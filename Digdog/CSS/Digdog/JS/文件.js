add21079b1cce0266b4fa5330f4c5c3d3235536a
雪人 <165768944+linesay@users.noreply.github.com>
2024-05-14 18:57:51 +0800

    Delete Digdog directory

--- Digdog/CSS/Digdog/JS/文件.js
+++
@@ -1,9 +0,0 @@
-function loadAvatar(event) {
-    var reader = new FileReader();
-    reader.onload = function(){
-      var avatar = document.getElementById('avatar');
-      avatar.src = reader.result;
-    };
-    reader.readAsDataURL(event.target.files[0]);
-  }
-  
